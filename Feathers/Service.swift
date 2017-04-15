e097a89f7d69e4dd5a0c9ee9b6e29b87ccf0b207
Brendan Conron <conronb@gmail.com>
2017-04-15 13:21:09 -0700

    added feathers service class

---
+++ Feathers/Service.swift
@@ -0,0 +1,45 @@
+//
+//  Service.swift
+//  Feathers
+//
+//  Created by Brendan Conron on 4/15/17.
+//  Copyright © 2017 Swoopy Studios. All rights reserved.
+//
+
+import UIKit
+
+final public class Service {
+
+    public let provider: Provider
+    public let path: String
+
+    internal init(provider: Provider, path: String) {
+        self.provider = provider
+        self.path = path
+    }
+
+    public final func find(parameters: [AnyHashable: Any]) {
+
+    }
+
+    public final func get(id: String, parameters: [AnyHashable: Any] = [:]) {
+
+    }
+
+    public final func create(data: [AnyHashable: Any], parameters: [AnyHashable: Any] = [:]) {
+
+    }
+
+    public final func update(id: String, data: [AnyHashable: Any], parameters: [AnyHashable: Any] = [:]) {
+
+    }
+
+    public final func patch(id: String, data: [AnyHashable: Any], parameters: [AnyHashable: Any] = [:]) {
+
+    }
+
+    public final func remove(id: String, parameters: [AnyHashable: Any] = [:]) {
+
+    }
+    
+}
