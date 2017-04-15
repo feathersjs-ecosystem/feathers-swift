73de30db12ef5ad7ee858e433c6f59b1384d191f
Brendan Conron <conronb@gmail.com>
2017-04-15 13:01:16 -0700

    fixed repository issues

---
+++ Feathers/Feathers.h
@@ -0,0 +1,19 @@
+//
+//  Feathers.h
+//  Feathers
+//
+//  Created by Brendan Conron on 4/15/17.
+//  Copyright © 2017 Swoopy Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Feathers.
+FOUNDATION_EXPORT double FeathersVersionNumber;
+
+//! Project version string for Feathers.
+FOUNDATION_EXPORT const unsigned char FeathersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feathers/PublicHeader.h>
+
+
