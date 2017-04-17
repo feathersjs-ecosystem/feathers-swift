3619660327c5f4a5c31a826d56da48fddb4d3b31
Brendan Conron <conronb@gmail.com>
2017-04-16 17:53:04 -0700

    added reactive swift + socket io

---
+++ Feathers/Service+ReactiveSwift.swift
@@ -0,0 +1,9 @@
+//
+//  Service+ReactiveSwift.swift
+//  Feathers
+//
+//  Created by Brendan Conron on 4/16/17.
+//  Copyright © 2017 Swoopy Studios. All rights reserved.
+//
+
+import Foundation
---
+++ Feathers/SocketProvider.swift
@@ -0,0 +1,13 @@
+//
+//  SocketProvider.swift
+//  Feathers
+//
+//  Created by Brendan Conron on 4/16/17.
+//  Copyright © 2017 Swoopy Studios. All rights reserved.
+//
+
+import UIKit
+
+class SocketProvider: NSObject {
+
+}
