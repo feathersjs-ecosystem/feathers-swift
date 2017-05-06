7e95882d95620bc4ee7c81b19bc369823318dab6
Brendan Conron <conronb@gmail.com>
2017-05-06 14:08:09 -0700

    file clean up

---
+++ Feathers/Feathers+ReactiveSwift.swift
@@ -0,0 +1,31 @@
+//
+//  Feathers+ReactiveSwift.swift
+//  Feathers
+//
+//  Created by Brendan Conron on 5/5/17.
+//  Copyright © 2017 Swoopy Studios. All rights reserved.
+//
+
+import Foundation
+import ReactiveSwift
+
+extension Reactive where Base: Feathers {
+
+    public func authenticate(_ credentials: [String: Any]) -> SignalProducer<Bool, FeathersError> {
+        return SignalProducer { [weak base = base] observer, disposable in
+            guard let vBase = base else {
+                observer.sendInterrupted()
+                return
+            }
+            vBase.authenticate(credentials) { success, error in
+                if let error = error {
+                    observer.send(error: error)
+                    return
+                }
+                observer.send(value: success)
+                observer.sendCompleted()
+            }
+        }
+    }
+    
+}
