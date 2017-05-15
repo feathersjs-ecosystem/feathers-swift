b50fbba228d7fca5c175bcdf3653607d734cf905
Brendan Conron <conronb@gmail.com>
2017-05-15 02:05:44 -0700

    added reauth hook

---
+++ Feathers/Core/AuthenticationHooks.swift
@@ -0,0 +1,31 @@
+//
+//  AuthenticationHooks.swift
+//  Feathers
+//
+//  Created by Brendan Conron on 5/15/17.
+//  Copyright © 2017 Swoopy Studios. All rights reserved.
+//
+
+
+/// Hook to reauthenticate the application if it becomes unauthorized
+struct ReauthenticationHook: Hook {
+
+    func run(with hookObject: HookObject, _ next: @escaping (HookObject) -> ()) {
+        var object = hookObject
+        let app = object.app
+        if let error = object.error as? FeathersError, let accessToken = app.authenticationStorage.accessToken, error == .notAuthenticated {
+            app.authenticate([
+                "strategy": app.authenticationConfiguration.jwtStrategy,
+                "accessToken": accessToken
+            ]) { token, error in
+                if let error = error {
+                    object.error = error
+                }
+                next(object)
+            }
+        } else {
+            next(object)
+        }
+    }
+
+}
