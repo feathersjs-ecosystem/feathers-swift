32e6c15834ca4cb6a395588a9f18dab2f2b9bfd3
Brendan Conron <conronb@gmail.com>
2017-05-18 21:02:25 -0700

    added brief service spec

--- FeathersTests/ServiceSpec.swift
+++ FeathersTests/ServiceSpec.swift
@@ -8,9 +8,43 @@
 
 import Quick
 import Nimble
+import Feathers
+import PromiseKit
 
 class ServiceSpec: QuickSpec {
 
-    
+    override func spec() {
+
+        describe("Service") {
+
+            var app: Feathers!
+            var service: Service!
+
+            beforeEach {
+                app = Feathers(provider: StubProvider(data: ["name": "Bob"]))
+                service = app.service(path: "users")
+            }
+
+            it("should stub the request") {
+                var error: Error?
+                var response: Response?
+                var data: [String: String]?
+                service.request(.find(parameters: nil))
+                    .then { value -> Promise<Void> in
+                        response = value
+                        data = value.data.value as? [String: String]
+                        return Promise(value: ())
+                    }.catch {
+                        error = $0
+                }
+                expect(error).toEventually(beNil())
+                expect(response).toEventuallyNot(beNil())
+                expect(data).toEventuallyNot(beNil())
+                expect(data).to(equal(["name": "Bob"]))
+            }
+
+        }
+
+    }
     
 }
