98d1918382d5e117f6db8bc72d1907b7e36b1df1
Brendan Conron <conronb@gmail.com>
2018-06-12 12:28:48 -0400

    added package resolved

--- Package.swift
+++ Package.swift
@@ -2,18 +2,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "FeathersSwift",
+    name: "Feathers",
     products: [
-        .library(name: "FeathersSwift", targets: ["FeathersSwift"]),
+        .library(name: "Feathers", targets: [
+          "Feathers"
+        ])
     ],
     dependencies: [
-        .package(url: "https://github.com/antitypical/ReactiveSwift.git", from: "3.1.0"),
+        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "3.1.0"),
+        .package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "11.0.0"),
         .package(url: "https://github.com/Quick/Quick.git", from: "1.3.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "7.1.2"),
     ],
     targets: [
-        .target(name: "FeathersSwift", dependencies: ["Result"], path: "Core"),
-        .testTarget(name: "FeathersSwiftTests", dependencies: ["Feathers", "Quick", "Nimble"]),
+        .target(name: "Feathers", dependencies: [
+          "KeychainSwift",
+          "ReactiveSwift"
+        ], path: "Feathers"),
+        .testTarget(name: "FeathersTests", dependencies: [
+          "Feathers",
+          "Quick",
+          "Nimble"], path: "FeathersTests")
     ],
     swiftLanguageVersions: [4]
 )
