faff355aaf76509df05ae87ef7f726c8e1975877
Brendan Conron <conronb@gmail.com>
2018-06-12 14:02:46 -0400

    fixed version number

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "3.1.0"),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "11.0.0"),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "3.1.1"),
         .package(url: "https://github.com/Quick/Quick.git", from: "1.3.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "7.1.2"),
     ],
