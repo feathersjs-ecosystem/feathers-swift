dfffd21fa576cfbb51a817063071a68f55ee7d2b
Ostap Holub <holub.ostap@gmail.com>
2019-03-25 17:31:44 +0200

    add query serializer util class

---
+++ Feathers/QuerySerializer.swift
@@ -0,0 +1,17 @@
+//
+//  QuerySerializer.swift
+//  Feathers-iOS
+//
+//  Created by Ostap Holub on 3/25/19.
+//  Copyright © 2019 Swoopy Studios. All rights reserved.
+//
+
+import Foundation
+
+public class QuerySerializer {
+    
+    public class func serialize() -> [String: Any] {
+        return [:]
+    }
+    
+}
